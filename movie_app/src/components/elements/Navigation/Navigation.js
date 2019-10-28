043f239600659ed589882758fea0061b4a5e258c
Romane G <romane71193@gmail.com>
2019-10-27 21:01:19 -0400

    boilerplate

--- movie_app/src/components/elements/Navigation/Navigation.js
+++ movie_app/src/components/elements/Navigation/Navigation.js
@@ -1,3 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import './Navigation.css'
+
+
+import React from 'react'
+
+export default Navigation = () => {
+  return (
+    <div>
+      Navigation
+    </div>
+  )
+}
